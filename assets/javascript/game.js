30ee05638393f170a3fa3277869f1759ec2b3101
egretsch <38367979+egretsch@users.noreply.github.com>
2018-09-03 03:20:51 -0700

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,16 @@
-    var wins = 0;
+//Gives a value for wins and losses and maximum value errors
+var wins = 0;
 var losses = 0;
-
 var maxErrors = 12;
-
+//Print information to that page from the game logic
 var wordDisplayLettersElement = document.getElementById("word-display-letters");
 var guessedLettersElement = document.getElementById("guessed-letters");
 var errorCountElement = document.getElementById("error-count");
 var winCountElement = document.getElementById("win-count");
 var lossCountElement = document.getElementById("loss-count");
-
 var blinkElements = document.getElementsByClassName("blinking");
 var alertLineElements = document.getElementsByClassName("alert-line");
-
+//Valid letters that can be guessed
 var validGuesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var pressAnyKeyToStart = [
@@ -27,7 +26,7 @@ var emptyAlert = [
 ];
 
 var game = new Hangman();
-
+//Tracks letters guessed by user and incorrect letters if they already exist
 document.onkeyup = function (event) {
     var userGuess = event.key;
 
@@ -40,7 +39,7 @@ document.onkeyup = function (event) {
         game.updatePageData();
     }
 }
-
+//Errors left or game over criteria are met game is ended
 window.setInterval(function () {
     if (blinkElements.length > 0) {
         if (game.guessedLetters.length === 0 || game.gameOver) {
@@ -60,7 +59,7 @@ window.setInterval(function () {
         }
     }
 }, 750);
-
+//Controls the word bank
 function Hangman() {
     this.wordList = [
         'batman',
@@ -88,10 +87,13 @@ function Hangman() {
         'guts',
         'kenshiro'
     ]
-
+    //Randomly selects word from word list
     this.word = this.wordList[Math.floor(Math.random() * this.wordList.length)];
+    //Hooray for guest letters
     this.guessedLetters = [];
+   //Gives the number of errors that have been made
     this.errors = 0;
+    //Turns letters into an array so they are visible on the page
     this.visibleLetters = [];
     this.gameOver = false;
     this.alertLines = emptyAlert;
@@ -99,7 +101,7 @@ function Hangman() {
         this.visibleLetters[i] = (false);
     }
 }
-
+//Increases losses versus wins as well as when you lose or win
 Hangman.prototype.checkGuess = function (char) {
     this.guessedLetters.push(char);
 
@@ -128,15 +130,16 @@ Hangman.prototype.checkGuess = function (char) {
 
     game.updatePageData();
 };
-
+//Main controller for game logic
 Hangman.prototype.updatePageData = function () {
     var tempString = "";
+    //Turns all letters into underscores
     for (var i = 0; i < this.visibleLetters.length; i++) {
         tempString += ((this.visibleLetters[i] || this.gameOver) ? this.word.charAt(i).toUpperCase() : "_");
         if (i < (this.visibleLetters.length - 1)) tempString += " ";
     }
     wordDisplayLettersElement.textContent = tempString;
-
+    
     tempString = "";
     for (var i = 0; i < this.guessedLetters.length; i++) {
         tempString += (this.guessedLetters[i].toUpperCase());
@@ -146,7 +149,7 @@ Hangman.prototype.updatePageData = function () {
         tempString += " ";
     }
     guessedLettersElement.textContent = tempString;
-
+    
     tempString = this.errors + " / " + maxErrors;
     for (var i = tempString.length; i < 32; i++) {
         tempString += " ";
@@ -168,10 +171,10 @@ Hangman.prototype.updatePageData = function () {
     for (var i = 0; i < blinkElements.length; i++) {
         blinkElements[i].textContent = (this.gameOver ? pressAnyKeyToReset[i] : pressAnyKeyToStart[i]);
     }
-
+    
     for (var i = 0; i < alertLineElements.length; i++) {
         alertLineElements[i].textContent = (this.alertLines[i]);
     }
 }
-
+//Starts the entire game to call the controller logic
 game.updatePageData();
